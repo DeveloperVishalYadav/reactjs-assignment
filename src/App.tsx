8e2f7cd678db4b3f50e82923037d71b1b53cced0
Vishal Yadav <vishal@minilive.in>
2023-04-25 14:37:57 +0530

    react-app

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,20 @@ const apiUserData = "https://randomuser.me/api";
  
  
 function App() {
-  const [userData, setData]=useState([]);
+  const [userFName, setFName]=useState([]);
+  const [userLName, setLName]=useState([]);
+  const [userEmail, setEmail]=useState([]);
 
   const getUserData = async ()=> {
     const {data} = await axios.get(apiUserData);
     //console.log(data);
 
     const userData = data.results[0]; 
-    setData(userData) 
+     
+    setEmail(userData.email) 
+    setFName(userData.name?.first)
+    setLName( userData.name?.last)
+
    }
 
   useEffect(()=>{ 
@@ -30,22 +36,19 @@ function App() {
 
 <section className="w-auto mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg text-center">
     
-    <div className=" mt-6 w-fit mx-auto">
-        <img src={userData.picture?.large} className="rounded-full w-28 " alt="profile picture"/>
+     <div className="text-white mt-6 w-fit mx-auto">
+       <p> User Details</p>
     </div>
-
+ 
     <div className="mt-8 ">
-        <h2 className="text-white font-bold text-2xl tracking-wide"> {userData.name?.title} {userData.name?.first} {userData.name?.last}</h2>
+        <h2 className="text-white font-bold text-2xl tracking-wide">Name : {userFName} {userLName}</h2>
     </div>
     <p className="text-white  font-semibold mt-2.5" >
-    Email : {userData.email}
+    Email : {userEmail}
     </p>
 
      
-    <div className="mt-3 text-white text-sm">
-    <p  >Address : {userData.location?.street?.name}, {userData.location?.city}, {userData.location?.country},  {userData.location?.postcode}.</p> 
-       
-    </div>
+    
     <button className='bg-blue-500 mt-10 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded' onClick={getUserData}>Get other user </button>
 </section>
 
